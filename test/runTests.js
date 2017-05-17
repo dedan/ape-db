310fb8bfab8e1a6ccacc934b87bb9451168d15b4
Stephan Gabler <stephan.gabler@gmail.com>
2017-05-17 10:09:57 +0700

    Fix `npm run test-watch` command

--- test/runTests.js
+++ test/runTests.js
@@ -6,4 +6,9 @@ const pattern = process.argv[2] === 'e2e'
   ? `test${s}e2e${s}.+\\.spec\\.js`
   : `test${s}(?!e2e${s})[^${s}]+${s}.+\\.spec\\.js$`;
 
-spawn.sync(path.normalize('./node_modules/.bin/jest'), [pattern], { stdio: 'inherit' });
+if (process.argv[2] === '--watch') {
+  spawn.sync(path.normalize('./node_modules/.bin/jest'), [pattern, '--watch'], { stdio: 'inherit' });
+} else {
+  spawn.sync(path.normalize('./node_modules/.bin/jest'), [pattern], { stdio: 'inherit' });
+}
+
