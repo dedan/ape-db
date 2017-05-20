6a978187ae44ab40231282a546587f780251a683
Stephan Gabler <stephan.gabler@gmail.com>
2017-05-20 11:23:50 +0700

    Show pages in a table and get rid of the old global styling

--- app/components/Catalog.js
+++ app/components/Catalog.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
 import path from 'path'
 import { Link } from 'react-router-dom';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 export default class Catalog extends Component {
 
@@ -9,15 +17,33 @@ export default class Catalog extends Component {
 
     return <div>
       <h1>Catalog</h1>
+      <Table>
+        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+          <TableRow>
+            <TableHeaderColumn>Image</TableHeaderColumn>
+            <TableHeaderColumn>Book</TableHeaderColumn>
+            <TableHeaderColumn>Page</TableHeaderColumn>
+            <TableHeaderColumn>Link</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false}>
+          {allPages.map(page => {
+            const filePath = 'file://' + page.original
+            const key = [page.book, page.page].join('-')
+            return <TableRow key={key}>
+              <TableRowColumn>
+                {page.original ? <img src={filePath} width="100" height="100" /> : null}
+              </TableRowColumn>
+              <TableRowColumn>{page.book}</TableRowColumn>
+              <TableRowColumn>{page.page}</TableRowColumn>
+              <TableRowColumn>
+                <Link to={`/current-page/${page.book}/${page.page}`}>Edit</Link>
+              </TableRowColumn>
+            </TableRow>
+          })}
+        </TableBody>
+      </Table>
       <ul>
-        {allPages.map(page => {
-          const filePath = 'file://' + page.original
-          return <li key={page.original}>
-            <img src={filePath} width="100" height="100" />
-            {page.original }
-            <Link to={`/current-page/${page.book}/${page.page}`}>Edit</Link>
-          </li>
-        })}
       </ul>
     </div>
   }
